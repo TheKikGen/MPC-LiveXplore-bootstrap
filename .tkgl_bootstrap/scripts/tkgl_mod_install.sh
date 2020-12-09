8c153350b032de01aff6ecebcec4f02b90c8a0ad
TheKikGen <TheKikGen@gmail.com>
2020-12-09 01:45:43 +0100

    lib directory added and updated install script

--- .tkgl_bootstrap/scripts/tkgl_mod_install.sh
+++ .tkgl_bootstrap/scripts/tkgl_mod_install.sh
@@ -6,28 +6,21 @@
 #   _|  _| |_|\___| _|\_\_|_|\_\\____|\___|_|  _| _____|\__,_|_.__/ ____/
 #
 # BOOTSTRAP INSTALL script for MPC device.
+# install      : setup some directories and permissions on the filesystem
 
-# This shell script will :
-# - Add the tkgl bin to the path
-# - create and mount an overlay for Arp patterns and progressions on this sd card.
-
-
-TKGL_ROOT=$(dirname $(readlink -f "$0"))/..
-TKGL_SCRIPT=$TKGL_ROOT/scripts
-TKGL_BIN=$TKGL_ROOT/bin
-TKGL_CONF=$TKGL_ROOT/conf
-TKGL_LOGS=$TKGL_ROOT/logs
-TKGL_LIB=$TKGL_ROOT/lib
+source $TKGL_PATH_FILE
 
 # creates all the directories again because empty dirs can be missing
-cd $TKGL_ROOT
+
 echo "*** The KiGenLabs bootstrap install script"
 echo "Directories creation..."
-mkdir -p $TKGL_SCRIPT>>/dev/null
-mkdir -p $TKGL_BIN>>/dev/null
-mkdir -p $TKGL_CONF>>/dev/null
-mkdir -p $TKGL_LOGS>>/dev/null
-mkdir -p $TKGL_LIB>>/dev/null
+mkdir -p $TKGL_SCRIPT
+mkdir -p $TKGL_BIN
+mkdir -p $TKGL_CONF
+mkdir -p $TKGL_LOGS
+mkdir -p $TKGL_LIB
+
+echo "*** $0 module">>$TKGL_LOG
 
 echo "Changing permission and ownership in /bin /scripts..."
 
@@ -35,15 +28,12 @@ chown root:root $TKGL_BIN/*
 chmod 755 $TKGL_BIN/*
 chown root:root $TKGL_SCRIPT/*
 chmod 755 $TKGL_SCRIPT/*
-chown root:root $TKGL_LIB/*                         
-chmod 755 $TKGL_LIB/* 
+chown root:root $TKGL_LIB/*
+chmod 755 $TKGL_LIB/*
 
-echo "*** Done !" 
-echo "*** Please check below :' 
-echo "*** - directries existence, 
-echo "*** - permissions and ownership by root"
+ls $TKGL_ROOT>>$TKGL_LOG
+ls -l $TKGL_BIN>>$TKGL_LOG
+ls -l $TKGL_SCRIPT>>$TKGL_LOG
 
-ls $TKGL_ROOT
-ls -l $TKGL_BIN
-ls -l $TKGL_SCRIPT
-
+echo "*** Done !"
+echo "*** Please check $TKGL_LOG"

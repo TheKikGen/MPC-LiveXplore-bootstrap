260b778c411b63ced0e690f976ab5be4721ca7d3
TheKikGen <35703467+TheKikGen@users.noreply.github.com>
2022-02-13 12:45:28 +0100

    Mounted usb disks are now availables within the chroot

--- tkgl_bootstrap_[ProjectData]/modules/mod_iamforce/tkgl_mod_iamforce.sh
+++ tkgl_bootstrap_[ProjectData]/modules/mod_iamforce/tkgl_mod_iamforce.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
-# __ __| |           |  /_) |     ___|             |           |
-#    |   __ \   _ \  ' /  | |  / |      _ \ __ \   |      _` | __ \   __|
-#    |   | | |  __/  . \  |   <  |   |  __/ |   |  |     (   | |   |\__ \
-#   _|  _| |_|\___| _|\_\_|_|\_\\____|\___|_|  _| _____|\__,_|_.__/ ____/
+# _ __| |           |  /) |     ___|             |           |
+#    |   _ \   _ \  ' /  | |  / |      _ \ _ \   |      ` | _ \   __|
+#    |   | | |  _/  . \  |   <  |   |  __/ |   |  |     (   | |   |\_ \
+#   |  _| |_|\___| _|\_\_|_|\_\\____|\___|_|  _| _____|\_,_|_.__/ ____/
 #
 # BOOTSTRAP script for MPCMAPPER LD_PRELOAD LIBRARY.
 
@@ -70,12 +70,7 @@ echo "Mounting point of the Force OS img      : $ROOT_DIR"  >> $TKGL_LOG
 echo "Root fs Force img file (ro)             : $ROOTFS_IMG_NAME" >>  $TKGL_LOG
 echo "-------------------------------------------------------------------------" >>  $TKGL_LOG
 
-# az01-internal partitions
-AZ01_INTERNAL_PART=/dev/mmcblk2p7
-AZ01_INERNAL_SD_PART=/dev/mmcblk0p1
-
 # Settings directory. Used to protect the host settings
-AZ01_INTERNAL_SETTINGS_MPC=/media/az01-internal/Settings/MPC
 TKGL_SETTINGS_MPC=$ASSETS_DIR/Settings_MPC
 
 # Virtual sd card
@@ -86,14 +81,6 @@ MPC_CRASHINFO=/media/az01-internal/Settings/MPC/*.crashinfo
 
 # Message displayed in a popup when the MPC app start
 MPC_MESSAGEINFO=/media/az01-internal/Settings/MPC/MPC.message
-
-# $ dd if=/dev/zero of=~/theFile.img bs=1k count=1034064
-# $ mkfs.ext4 ~/theFile.img
-# cp -a to copy
-# internal sd uuid : 6df1ed3a-f152-49ee-b7de-b3d9662ca920
-# mkfs.ext4 ./sdcard.img -U 6df1ed3a-f152-49ee-b7de-b3d9662ca920 -E offset=$((8192*512))
-# mount -o offset=$((8192*512)) ./sdcard.img /tmp/mnt1
-
 
 # Prepare the chroot "Sandbox" -------------------------------------------------
 
@@ -109,36 +96,30 @@ then
    exit 1
 fi
 
-# Check rw image before mounting
-#e2fsck -pf $ROOTFS_IMG_NAME
-
-# Mount image
+# Mount force image, read only
 mkdir -p $ROOT_DIR
 echo "Mounting rootfs img : mount -o ro $ROOTFS_IMG_NAME $ROOT_DIR" >> $TKGL_LOG
 mount -o ro $ROOTFS_IMG_NAME $ROOT_DIR
 
-# Create the media binding directory
+# Make the media binding directory
 echo "Create $ROOT_DIR_MEDIA media binding" >> $TKGL_LOG
 mkdir -p $ROOT_DIR_MEDIA
-# change permissions in the rootfs img
-chmod 1777 $ROOT_DIR_MEDIA
-# bind internal to our rootfs to get a full rw media directory
-mount --bind $ROOT_DIR_MEDIA $ROOT_DIR/media
-
-# bind the host az01-internal, and bind again to protect MPC settings
-echo "Create az01-internal binding at $TKGL_SETTINGS_MPC" >> $TKGL_LOG
-
-mkdir -p $ROOT_DIR/media/az01-internal
-mount --bind /media/az01-internal $ROOT_DIR/media/az01-internal
+mkdir -p $TKGL_AZ01_INTERNAL_SD
 mkdir -p $TKGL_SETTINGS_MPC
-mount --bind $TKGL_SETTINGS_MPC $AZ01_INTERNAL_SETTINGS_MPC
 
+# Bind our media directory with the true /media
+mount --rbind /media $ROOT_DIR_MEDIA
+
+# Bind our pseudo sd card over our media directory (beeing /media)
+mount --rbind $TKGL_AZ01_INTERNAL_SD  $ROOT_DIR_MEDIA/az01-internal-sd
+
+# Bind to separate settings from our host settings
+mount --rbind $TKGL_SETTINGS_MPC $ROOT_DIR_MEDIA/az01-internal/Settings/MPC
+
+# Finally bind with the chroot /media
+mount --rbind $ROOT_DIR_MEDIA $ROOT_DIR/media
 
 # Sanitary checks -------------------------------------------------------------
-
-# clear pseudo-mounted disk in chroot  /media but az01-*
-# We use rmdir here to avoid rm side effect. Dirs are empty when unbinded.
-rmdir $(find $ROOT_DIR/media/* | grep -v az01-)
 
 # Clear crash files
 rm $ROOT_DIR$MPC_CRASHINFO
@@ -174,50 +155,25 @@ upperdir=$ROOT_DIR/media/az01-internal/system/var/overlay,\
 workdir=$ROOT_DIR/media/az01-internal/system/var/.work \
 $ROOT_DIR/var>>$TKGL_LOG
 
-# Bind user partitions in the chroot space-------------------------
-
-# pseudo internal sd card
-mkdir -p $TKGL_AZ01_INTERNAL_SD
-mkdir -p $ROOT_DIR/media/az01-internal-sd
-mount --bind $TKGL_AZ01_INTERNAL_SD  $ROOT_DIR/media/az01-internal-sd>>$TKGL_LOG
-
-# search for mounted usb disks but the one containing our chroot to avoid bad manipulation...
-MOUNT_LIST=$(lsblk -o MOUNTPOINT,PATH,SUBSYSTEMS,TYPE | grep '\S*usb.*part' | grep -Ev "$MOUNT_POINT" | cut -d' ' -f1)
-
-if [ "x$MOUNT_LIST" != "x" ]
-then
- for DISK_MOUNT_PATH in $MOUNT_LIST ; do
-  # bind mounted usb disk into the chroot
-  if [ "x$DISK_MOUNT_PATH" != "x" ]
-  then
-    echo "Bind $DISK_MOUNT_PATH  to $ROOT_DIR$DISK_MOUNT_PATH">>$TKGL_LOG
-    chmod 777 $DISK_MOUNT_PATH
-    mkdir -p $ROOT_DIR$DISK_MOUNT_PATH
-    chmod 777 $ROOT_DIR$DISK_MOUNT_PATH
-    mount --rbind $DISK_MOUNT_PATH  $ROOT_DIR$DISK_MOUNT_PATH
-  fi
- done
-fi
-
 # start to chroot in the Force image -------------------------------------------
 
-# Make our bootstrap space in the chroot at /etc/tkgl_bootstrap (overlay)
+# Make our bootstrap space in the chroot at /etc/tkgl_bootstrap
 mkdir -p $ROOT_DIR$CHROOT_BOOTSTRAP
 mount --rbind $TKGL_ROOT $ROOT_DIR$CHROOT_BOOTSTRAP>>$TKGL_LOG
 
 # Prepare a welcome message
 echo "Welcome to The Kikgen Labs world !">$ROOT_DIR$MPC_MESSAGEINFO
 
-
 # Prepare the launch script within the chroot
-# chroot into our image
+# then chroot into our image
 
 cat << EOF | chroot $ROOT_DIR
 ulimit -S -s 1024
 LD_PRELOAD=$CHROOT_BOOTSTRAP/lib/tkgl_mpcmapper.so exec /usr/bin/MPC $ARGV $TKGL_ARGV
 #EOF
 
-# shut down
+shut down
+
 # umount fs --------------------------------------------------------------------
 # umount $ROOT_DIR/tmp
 # umount $ROOT_DIR/dev
